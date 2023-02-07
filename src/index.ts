284ff4b2229d8cb6c58dcbb932c49ac3120b4001
MrYeZiqing <2019873029@qq.com>
2023-02-07 15:19:09 +0800

    feat: 添加统一导出入口

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// browser
+export { getUrlParam } from './browser/getUrlParam';
+export { getUrlParams } from './browser/getUrlParams';
+export { setUrlParams } from './browser/setUrlParams';
+// language
+export { attachPropertiesToComponent } from './language/attachPropertiesToComponent';
+export { mergeProps } from './language/mergeProps';
+export { omit } from './language/omit';
+// type
+export { getType } from './type/getType';
+export { isArray } from './type/isArray';
+export { isBase64 } from './type/isBase64';
+export { isBlob } from './type/isBlob';
+export { isBoolean } from './type/isBoolean';
+export { isEmpty } from './type/isEmpty';
+export { isEmptyArray } from './type/isEmptyArray';
+export { isEmptyObject } from './type/isEmptyObject';
+export { isEmptyString } from './type/isEmptyString';
+export { isFile } from './type/isFile';
+export { isFunction } from './type/isFunction';
+export { isNull } from './type/isNull';
+export { isNumber } from './type/isNumber';
+export { isObject } from './type/isObject';
+export { isRegExp } from './type/isRegExp';
+export { isString } from './type/isString';
+export { isUndefined } from './type/isUndefined';
